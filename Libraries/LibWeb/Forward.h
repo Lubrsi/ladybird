bf53a7f2259fa97eaca7a63f09b403daffbbc7cb
Luke Wilde <luke@ladybird.org>
2024-11-25 14:30:12 +0000

    LibWeb: Make PolicyContainer GC allocated
    This is required to store Content Security Policies, as their
    Directives are implemented as subclasses with overridden virtual
    functions. Thus, they cannot be stored as generic Directive classes, as
    it'll lose the ability to call overridden functions when they are
    copied.

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -561,6 +561,7 @@ struct POSTResource;
 struct ScrollOptions;
 struct ScrollToOptions;
 struct SerializedFormData;
+struct SerializedPolicyContainer;
 struct StructuredSerializeOptions;
 struct SyntheticRealmSettings;
 struct ToggleTaskTracker;
