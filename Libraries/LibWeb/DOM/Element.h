78f9c37dd7eb2190c80e6bccc9f3a532ad7154fa
Luke Wilde <luke@ladybird.org>
2024-12-02 12:33:52 +0000

    LibWeb: Track if element was created from token with dupe attributes
    This is required for CSP to ignore the nonce attribute to prevent
    duplicate attributes hijacking the attribute.
    
    See https://w3c.github.io/webappsec-csp/#security-nonce-hijacking

--- Libraries/LibWeb/DOM/Element.h
+++ Libraries/LibWeb/DOM/Element.h
@@ -398,6 +398,9 @@ public:
     void determine_proximity_to_the_viewport();
     bool is_relevant_to_the_user();
 
+    void set_had_duplicate_attribute_during_tokenization(Badge<HTML::HTMLParser>);
+    bool had_duplicate_attribute_during_tokenization() const { return m_had_duplicate_attribute_during_tokenization; }
+
 protected:
     Element(Document&, DOM::QualifiedName);
     virtual void initialize(JS::Realm&) override;
@@ -486,6 +489,13 @@ private:
     Array<CSSPixelPoint, 3> m_scroll_offset;
 
     bool m_in_top_layer { false };
+
+    // https://w3c.github.io/webappsec-csp/#is-element-nonceable
+    // AD-HOC: We need to know the element had a duplicate attribute when it was created from the HTML parser.
+    //         However, there currently isn't any specified way to do this, so we store a flag on the token, which is
+    //         then passed down to here. This is used by Content Security Policy to disable the nonce attribute if this
+    //         flag is set.
+    bool m_had_duplicate_attribute_during_tokenization { false };
 
     OwnPtr<CSS::CountersSet> m_counters_set;
 
