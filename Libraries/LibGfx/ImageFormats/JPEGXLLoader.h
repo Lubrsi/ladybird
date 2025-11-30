4964b931c75eb5f3f0dbad7b432ffaab8ccd0cab
Luke Wilde <luke@ladybird.org>
2025-11-30 17:33:05 +0000

    WIP on progressive-image-decoding

--- Libraries/LibGfx/ImageFormats/JPEGXLLoader.h
+++ Libraries/LibGfx/ImageFormats/JPEGXLLoader.h
@@ -14,8 +14,8 @@ class JPEGXLLoadingContext;
 
 class JPEGXLImageDecoderPlugin : public ImageDecoderPlugin {
 public:
-    static bool sniff(ReadonlyBytes);
-    static ErrorOr<NonnullOwnPtr<ImageDecoderPlugin>> create(ReadonlyBytes);
+    static bool sniff(NonnullRefPtr<Core::SeekableSharedMemoryStream> stream);
+    static ErrorOr<NonnullOwnPtr<ImageDecoderPlugin>> create(NonnullRefPtr<Core::SeekableSharedMemoryStream> stream);
 
     virtual ~JPEGXLImageDecoderPlugin() override;
 
